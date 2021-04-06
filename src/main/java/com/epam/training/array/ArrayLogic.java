e6168fee0bd91069125c8eae46a7483f3b0b0f48
KarolinaRi <k.ripinskyte@gmail.com>
2021-04-06 15:09:41 +0300

    Arrays task

--- src/main/java/com/epam/training/array/ArrayLogic.java
+++ src/main/java/com/epam/training/array/ArrayLogic.java
@@ -3,14 +3,80 @@ package com.epam.training.array;
 public class ArrayLogic {
 
     //TODO: write it using binary search
-    public boolean exists(Array array) {
-        throw new UnsupportedOperationException();
+    public boolean exists(Array array, int element) {
+        ArraySorter sorted = new ArraySorter();
+        array = sorted.sort(array);
+        int[] temp =  array.getItems();
+
+        int last = temp.length - 1;
+        int first = 0;
+        int middle = (first + last) / 2;
+
+        boolean result = false;
+        while(first <= last){
+            if(temp[middle] < element){
+                first = middle + 1;
+            }
+            else if(temp[middle] == element){
+                result = true;
+                //return true;
+            }
+            else{
+                last = middle - 1;
+            }
+            middle = (first + last) / 2;
+            if(first > last){
+                result = false;
+                //return false;
+            }
+        }
+        return result;
+        //throw new UnsupportedOperationException();
     }
 
     public Array findPrimeNumbers(Array array) {
-        throw new UnsupportedOperationException();
+        int n = array.getArrayLength();
+        int[] primeNumbers = new int[n];
+
+        int[] arr = array.getItems();
+        for (int i = 0; i < arr.length; i++) {
+            boolean isPrime = true;
+            for(int j = 2; j < arr[i]; j++){
+                if(arr[i] % j == 0){
+                    isPrime = false;
+                    n--;
+                    break;
+                }
+            }
+            if(isPrime){
+                for(int j = 0; j < n; j++){
+                    primeNumbers[j] = arr[i];
+                }
+            }
+        }
+        return new Array(primeNumbers);
     }
     
    //TODO: add other methods here
 
+    public int findMaxValue(Array array){
+        int[] arr = array.getItems();
+        int max = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public int findMinValue(Array array){
+        int[] arr = array.getItems();
+        int min = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
 }
